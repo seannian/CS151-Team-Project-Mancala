c6c4172157b244987d1b5bcf2d700ff0e110f1fb
Abdugafur Dalerzoda <64993553+Abdugafur111@users.noreply.github.com>
2022-11-22 01:40:34 -0800

    Update DecoratedBoard.java

--- DecoratedBoard.java
+++ DecoratedBoard.java
@@ -1,17 +1,59 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 public class DecoratedBoard extends JPanel {
-    int stoneNumber;
+    private int x = 450;
+    private int y = 215;
+    private int width = 80;
+    private int[] arr = new int[12];
+    private Point mousePoint;
+    private ArrayList<PitShape> pitShapes;
+
 
     public DecoratedBoard(int stones) {
-        stoneNumber = stones;
+        //stoneNumber = stones;
+        //setAllPitStones(stoneNumber);
+        pitShapes = new ArrayList<>();
+        addMouseListener(new MousePressedListener());
+
+    }
+
+    private class MousePressedListener extends MouseAdapter {
+        @Override
+        public void mousePressed(MouseEvent event)
+        {
+             mousePoint = event.getPoint();
+            for (PitShape s : pitShapes)
+            {
+                if (s.contains(mousePoint)){
+                    setPitStone(0,0);
+                    setPitStone(1,5);
+                    setPitStone(2,5);
+                    setPitStone(3,5);
+                    setPitStone(4,5);
+                    setPitStone(5,4);
+                    setPitStone(6,4);
+                    setPitStone(7,4);
+                    setPitStone(8,4);
+                    setPitStone(8,4);
+                    setPitStone(10,4);
+                    setPitStone(11,4);
+                }
+            }
+
+            repaint();
+        }
     }
 
     public void paintComponent(Graphics g) {
+
 
 
         super.paintComponent(g);
@@ -23,22 +65,26 @@ public class DecoratedBoard extends JPanel {
         g2.setPaint(Color.BLACK);
         g2.setPaint(Color.RED);
 
+//
+//
+//        PitShape pitShape = new PitShape(0,0,100);
+//        pitShape.draw(g2,0,0,1,200,Color.RED);
+
         /** Pits */
+
         for (int i = 0; i < 12; i++) {
+            PitShape pitShape;
             g2.setPaint(Color.BLACK);
             g2.setStroke(new BasicStroke(5));
-
             if (i < 6){
-
-                  g2.drawOval(450 + i*100, 215, 95, 95);
-                  g2.setPaint(new Color(204,204,204));
-                  g2.fillOval(450 + i*100, 215, 95, 95);
+                pitShape = new PitShape(i,x,y,width,i);
+                pitShape.draw(g2,x,y,i,width,new Color(204,204,204));
+                pitShapes.add(pitShape);
             }
             else{
-                g2.drawOval(450 + (i-6)*100, 405, 95, 95);
-                g2.setPaint(new Color(102,102,102));
-                g2.fillOval(450 + (i-6)*100, 405, 95, 95);
-
+                pitShape = new PitShape(i,x,y+200,width,i-6);
+                pitShape.draw(g2,x,y+200,i-6,width,new Color(102,102,102));
+                pitShapes.add(pitShape);
             }
         }
 
@@ -68,38 +114,16 @@ public class DecoratedBoard extends JPanel {
 
         g2.setColor(new Color(255,102,102));
         /** Draw 3 stones */
-        if (stoneNumber == 3) {
-            for (int i = 0; i < 12; i++)
-            {
-                if (i < 6){
-                    g2.fillOval(468 + i*100, 240, 10, 10);
-                    g2.fillOval(482 + i*100, 240, 10, 10);
-                    g2.fillOval(496 + i*100, 240, 10, 10);
-                }
-                else {
-                    g2.fillOval(468 + (i-6)*100, 430, 10, 10);
-                    g2.fillOval(482 + (i-6)*100, 430, 10, 10);
-                    g2.fillOval(496 + (i-6)*100, 430, 10, 10);
-                }
+
+        for(int i=0;i<12;i++){
+            if (i < 6) {
+                drawStonesOfPit(g2, arr[i], 450 + i * 100, 240);
+            }else {
+                drawStonesOfPit(g2, arr[i], 450 + (i-6) * 100, 430);
             }
         }
-        else if (stoneNumber == 4) {
-            for (int i = 0; i < 12; i++)
-            {
-                if (i < 6){
-                    g2.fillOval(468 + i*100, 240, 10, 10);
-                    g2.fillOval(482 + i*100, 240, 10, 10);
-                    g2.fillOval(496 + i*100, 240, 10, 10);
-                    g2.fillOval(468 + i*100, 254, 10, 10);
-                }
-                else {
-                    g2.fillOval(468 + (i-6)*100, 430, 10, 10);
-                    g2.fillOval(482 + (i-6)*100, 430, 10, 10);
-                    g2.fillOval(496 + (i-6)*100, 430, 10, 10);
-                    g2.fillOval(468 + (i-6)*100, 444, 10, 10);
-                }
-            }
-        }
+
+
         g2.setPaint(Color.BLACK);
         g2.setBackground(new Color(102,102,102));
         /** Mancala */
@@ -125,8 +149,33 @@ public class DecoratedBoard extends JPanel {
         g2.setTransform(at2);
         g2.drawString("MANCALA A", 650, -2180);
     }
-
-    public void setStones(int stones) {
-        this.stoneNumber = stones;
+    public void drawStonesOfPit(Graphics2D g2, int numberOfStones,int xPit,int yPit){
+        int summer=0;
+        int stoneCounter = 0;
+        int y = yPit;
+        for(int i=0;i<numberOfStones;i++){
+            if(stoneCounter == 6){
+                y = y+14;
+                summer = 0;
+                stoneCounter = 0;
+            }
+            summer=summer+12;
+            g2.fillOval(xPit + summer, y, 10, 10);
+            stoneCounter++;
+        }
     }
+
+    public void setPitStone(int pitNumber,int numberOfStones){
+        arr[pitNumber] = numberOfStones;
+    }
+
+    public void setAllPitStones(int stoneNumber){
+        for(int i=0;i< arr.length;i++){
+            arr[i]=stoneNumber;
+        }
+    }
+
+
+
+
 }
