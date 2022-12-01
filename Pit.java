559b32f4d11164fc35c2b27151f3a802b3775e3a
seannian <88524922+seannian@users.noreply.github.com>
2022-12-01 13:22:09 -0800

    Update Pit.java

--- Pit.java
+++ Pit.java
@@ -6,7 +6,7 @@ public class Pit
 	private Player player;
 	private int pitNumber;
 	private Pit correlation;
-
+	
 	public Pit(int numberOfStones, int pitNumber)
 	{
 		stones = new ArrayList<Stone>();
@@ -16,22 +16,22 @@ public class Pit
 			stones.add(new Stone());
 		}
 	}
-
+	
 	public int getSize()
 	{
 		return stones.size();
 	}
-
+	
 	public int getPitNumber()
 	{
 		return pitNumber;
 	}
-
+	
 	public void emptyPit()
 	{
 		stones.clear();
 	}
-
+	
 	public void addStones(int number)
 	{
 		for(int i = 0; i < number; i++) 
@@ -39,24 +39,25 @@ public class Pit
 			stones.add(new Stone());
 		}
 	}
-
+	
 	public void setCorrelation(Pit pit)
 	{
 		this.correlation = pit;
 		pit.correlation = this;
 	}
-
+	
 	public Pit getCorrelation()
 	{
 		return correlation;
 	}
-
+	
 	public void setPlayer(Player player)
 	{
 		this.player = player;
 	}
-
+	
 	public Player getPlayer()
 	{
 		return player;
 	}
+}
