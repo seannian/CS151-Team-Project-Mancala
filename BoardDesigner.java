e6dcf19eb846419756400e7eff2ce2c594b01b80
Abdugafur Dalerzoda <64993553+Abdugafur111@users.noreply.github.com>
2022-12-01 03:12:13 -0800

    Update BoardDesigner.java

--- BoardDesigner.java
+++ BoardDesigner.java
@@ -4,7 +4,7 @@ public interface BoardDesigner {
     void paintComponent(Graphics g);
     void drawStonesOfPit(Graphics2D g2, int numberOfStones,int xPit,int yPit);
     void drawStonesOfMancala(Graphics2D g2, int numberOfStones,int xPit,int yPit);
-    void setPitStone(int pitNumber,int numberOfStones);
-    void setMancalaStone(int mancalaNum, int numberOfStones);
-    void setAllPitStones(int stoneNumber);
+    void setPitStone(int pitNumber);
+   // void setMancalaStone(int mancalaNum, int numberOfStones);
+   void setAllPitStones(int stoneNumber);
 }
