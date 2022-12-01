1076b027b702d59b4a08467ad368aa5cafbe23d0
Abdugafur Dalerzoda <64993553+Abdugafur111@users.noreply.github.com>
2022-12-01 01:44:57 -0800

    Update PitShape.java

--- PitShape.java
+++ PitShape.java
@@ -7,23 +7,36 @@ public class PitShape {
     private int y;
     private int width;
     private int pitNum;
-    private Player player;
-    
-    public PitShape(int pitNum,int x, int y, int width,int i, Player player)
+    private int numberOfStones;
+    public PitShape(int pitNum,int x, int y, int width,int i)
     {
-        this.x = x+i*100;
+        if(x==320){
+            this.x = x;
+        }else{
+            this.x = x+i*100;
+        }
         this.y = y;
         this.width = width;
         this.pitNum = pitNum;
-        this.player = player;
     }
 
     public void draw(Graphics2D g2,int x,int y,int i, int width, Color color)
     {
 
-        g2.drawOval(x+i*100, y, width, width);
-        g2.setPaint(color);
-        g2.fillOval(x+i*100, y, width, width);
+        if(width == 80) {
+            g2.drawOval(x + i * 100, y, width, width);
+            g2.setPaint(color);
+            g2.fillOval(x + i * 100, y, width, width);
+        }else if(width == 84){
+            g2.drawRect(x + i * 100, y, width, width);
+            g2.setPaint(color);
+            g2.fillRect(x + i * 100, y, width, width);
+        }
+        else{
+            g2.setBackground(color);
+            g2.drawRect(x, y, width, width+260);
+            g2.fillRect(x, y, width, width+260);
+        }
     }
 
     public int getX() {
@@ -49,19 +62,26 @@ public class PitShape {
     public void setWidth(int width) {
         this.width = width;
     }
-    
+
     public boolean contains(Point2D p)
     {
         return getX() <= p.getX() && p.getX() <= getX() + getWidth()
-                && getY() <= p.getY() && p.getY() <= getY() + getWidth();
+                && getY() <= p.getY() && p.getY() <= getY() + getWidth() / 2;
     }
 
     public int getPitNum(){
         return pitNum;
     };
-    
-    public Player getPlayer()
-    {
-    	return player;
+
+    public void setPitNum(int pitNum) {
+        this.pitNum = pitNum;
+    }
+
+    public int getNumberOfStones() {
+        return numberOfStones;
+    }
+
+    public void setNumberOfStones(int numberOfStones) {
+        this.numberOfStones = numberOfStones;
     }
 }
