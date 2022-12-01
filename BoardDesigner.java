c3291003b34ac5140cb06ff6478d9ea085362c5a
seannian <88524922+seannian@users.noreply.github.com>
2022-12-01 13:21:06 -0800

    Update BoardDesigner.java

--- BoardDesigner.java
+++ BoardDesigner.java
@@ -1,10 +1,16 @@
 import java.awt.*;
 
-public interface BoardDesigner {
+import javax.swing.event.ChangeListener;
+
+public interface BoardDesigner 
+{
     void paintComponent(Graphics g);
     void drawStonesOfPit(Graphics2D g2, int numberOfStones,int xPit,int yPit);
     void drawStonesOfMancala(Graphics2D g2, int numberOfStones,int xPit,int yPit);
     void setPitStone(int pitNumber);
-   // void setMancalaStone(int mancalaNum, int numberOfStones);
-   void setAllPitStones(int stoneNumber);
+    void setAllPitStones(int stoneNumber);
+    void undo();
+    void repaint();
+    void setBounds(int x, int y, int i, int j);
+    void attach(ChangeListener listener);
 }
