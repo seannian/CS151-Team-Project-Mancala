cdffbd6acc428be2dd6b7485f605e213c7f5c15a
seannian <88524922+seannian@users.noreply.github.com>
2022-12-01 20:07:45 -0800

    Update Stone.java

--- Stone.java
+++ Stone.java
@@ -1,4 +1,19 @@
+/**
+ * Fall 2022 CS151 Team Project
+ * Simple Mancala Board
+ * Instructor: Dr. Suneuy Kim
+ * 
+ * @author Sean Nian, Abdugafur Dalerzoda, Xianqiao Zhang, Aarushi  Gautam
+ * @version 1.0 12/1/2022
+ */
+
+/**
+ * Dummy class that creates empty stones that are to be added to the pit.
+ */
 public class Stone 
 {
+	/**
+	 * Creates an empty stone
+	 */
 	public Stone() {}
 }
